e99bed7343b9c24fbcbce72f33356c4e6448555e
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-07-24 18:17:10 +0700

    a

--- src/components/SidebarAdmin/index.tsx
+++ src/components/SidebarAdmin/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
-import { faMenu } from '@fortawesome/free-solid-svg-icons';
+import {} from '@fortawesome/free-solid-svg-icons';
 
 type Props = {};
 
@@ -16,117 +16,41 @@ const Sidebar = (props: Props) => {
           <div id="main-menu" className="main-menu collapse navbar-collapse">
             <ul className="nav navbar-nav">
               <li className="active">
-                <Link href={''}>
-                  <FontAwesomeIcon
-                    icon={faMenu}
-                    style={{ fontSize: 100, color: 'blue' }}
-                  />
-                </Link>
                 <a href="index.html">
                   <i className="menu-icon fa fa-laptop" />
                   Dashboard{' '}
                 </a>
               </li>
-              <li className="menu-title">UI elements</li>
+              <li className="menu-title">Elements</li>
               {/* /.menu-title */}
               <li className="menu-item-has-children dropdown">
-                <a href="#">
-                  {' '}
-                  <i className="menu-icon fa fa-cogs" />
-                  Components
-                </a>
-                <ul className="sub-menu children dropdown-menu">
-                  {' '}
-                  <li>
-                    <i className="fa fa-puzzle-piece" />
-                    <a href="ui-buttons.html">Buttons</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-id-badge" />
-                    <a href="ui-badges.html">Badges</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-bars" />
-                    <a href="ui-tabs.html">Tabs</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-id-card-o" />
-                    <a href="ui-cards.html">Cards</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-exclamation-triangle" />
-                    <a href="ui-alerts.html">Alerts</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-spinner" />
-                    <a href="ui-progressbar.html">Progress Bars</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-fire" />
-                    <a href="ui-modals.html">Modals</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-book" />
-                    <a href="ui-switches.html">Switches</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-th" />
-                    <a href="ui-grids.html">Grids</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-file-word-o" />
-                    <a href="ui-typgraphy.html">Typography</a>
-                  </li>
-                </ul>
+                <Link href="admin/products">
+                  <a>
+                    <i className="menu-icon fa fa-cogs" />
+                    Products
+                  </a>
+                </Link>
               </li>
               <li className="menu-item-has-children dropdown">
-                <a
-                  href="#"
-                  className="dropdown-toggle"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  {' '}
-                  <i className="menu-icon fa fa-table" />
-                  Tables
-                </a>
-                <ul className="sub-menu children dropdown-menu">
-                  <li>
-                    <i className="fa fa-table" />
-                    <a href="tables-basic.html">Basic Table</a>
-                  </li>
-                  <li>
-                    <i className="fa fa-table" />
-                    <a href="tables-data.html">Data Table</a>
-                  </li>
-                </ul>
+                <Link href="/">
+                  <a>
+                    <i className="menu-icon fa fa-cogs" />
+                    Users
+                  </a>
+                </Link>
               </li>
               <li className="menu-item-has-children dropdown">
-                <a
-                  href="#"
-                  className="dropdown-toggle"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  {' '}
-                  <i className="menu-icon fa fa-th" />
-                  Forms
-                </a>
-                <ul className="sub-menu children dropdown-menu">
-                  <li>
-                    <i className="menu-icon fa fa-th" />
-                    <a href="forms-basic.html">Basic Form</a>
-                  </li>
-                  <li>
-                    <i className="menu-icon fa fa-th" />
-                    <a href="forms-advanced.html">Advanced Form</a>
-                  </li>
-                </ul>
+                <Link href="/">
+                  <a>
+                    <i className="menu-icon fa fa-cogs" />
+                    News
+                  </a>
+                </Link>
               </li>
-              <li className="menu-title">Icons</li>
-              {/* /.menu-title */}
+
+              {/* -------------------------------------------------------------------------------------------- */}
+              {/* <li className="menu-title">Icons</li>
+
               <li className="menu-item-has-children dropdown">
                 <a
                   href="#"
@@ -207,8 +131,9 @@ const Sidebar = (props: Props) => {
                   </li>
                 </ul>
               </li>
+
               <li className="menu-title">Extras</li>
-              {/* /.menu-title */}
+
               <li className="menu-item-has-children dropdown">
                 <a
                   href="#"
@@ -235,7 +160,7 @@ const Sidebar = (props: Props) => {
                     <a href="pages-forget.html">Forget Pass</a>
                   </li>
                 </ul>
-              </li>
+              </li> */}
             </ul>
           </div>
           {/* /.navbar-collapse */}
