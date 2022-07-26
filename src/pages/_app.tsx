9e133b59f897163c763beaececa2e1dff40ad4ec
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-07-26 18:27:12 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,23 +5,23 @@ config.autoAddCss = false;
 
 import 'bootstrap/dist/css/bootstrap.css';
 //
-import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { AppPropsWithLayout } from '../models/Layout';
 import { SWRConfig } from 'swr';
-import instance from '../api/instance';
+// import instance from '../api/instance';
 
 import '../assets/admin/css/style.css';
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/main.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/index.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/responsive.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/quickviews_popup_cart.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/sidebar_style.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/collection_style.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/product_style.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/blog_article_style.scss.css?1656725435979'
-import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/cartpage.scss.css?1656725435979'
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/main.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/index.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/responsive.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/quickviews_popup_cart.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/sidebar_style.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/collection_style.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/product_style.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/blog_article_style.scss.css?1656725435979';
+import '../assets/client/bizweb.dktcdn.net/100/374/880/themes/748270/assets/cartpage.scss.css?1656725435979';
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const LayoutWrapper = Component.Layout ?? Layout;
   return (
